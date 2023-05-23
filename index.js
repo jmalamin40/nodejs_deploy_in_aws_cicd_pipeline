1291a384e42dbbef6d196d5a4b7681debca2fa6c
Mr.Al-Amin Hossain <jomaddaralamin69@gmail.com>
2023-05-23 15:16:19 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World deployed');
+  res.end('Node js project successfully deployed in aws');
 });
 
 server.listen(port, () => {
